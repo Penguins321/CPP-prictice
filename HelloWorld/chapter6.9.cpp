1097c34aa4758d63a54b6edc69f1a841adebdfe4
ouyang <3215399126@qq.com>
2024-06-12 17:14:54 +0800

    chapter6.9

--- HelloWorld/chapter6.9.cpp
+++ HelloWorld/chapter6.9.cpp
@@ -9,5 +9,10 @@ int main() {
 	cout << " is " << c << endl;
 	return 0;
 
+	/*const char x[2][20] = {"Jason ","at your service\n"};
+	const char* y = "Quillstone ";
 
+	for (int i = 0; i < 3; i++) {
+		cout << ((i < 2) ? !i ? x[i] : y : x[1]);
+	}*/
 }
