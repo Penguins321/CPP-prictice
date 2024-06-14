ee68d935718c8370d1fa86e8d294c43fd955b398
ouyang <3215399126@qq.com>
2024-06-14 21:51:27 +0800

    update

--- HelloWorld/chapter6_2.cpp
+++ HelloWorld/chapter6_2.cpp
@@ -1,29 +1,23 @@
 #include<iostream>
-#include<cctype>
 
 const int ArSize = 10;
 int main() {
 	using namespace std;
 
 	double donation[ArSize];
-	double temp;
 	double average = 0;
 	int i, count = 0;
 	double sum = 0;
-	int countup = 0;
+	int countAboveAverage = 0;
+	double temp;
 
 	cout << "enter your number: ";
-	cin >> temp;
-	for (i = 0; i < ArSize; i++) {
-		if (isdigit(temp)) {
-			donation[i] = temp;
-			sum += temp;
-			count++;
-			cin >> temp;
-		}
-		else
-			break;
+	while (count < ArSize && cin >> temp) {
+		donation[i] = temp;
+		count++;
+
 	}
+
 	// ±éÀúÊý×é
 	if (count != 0) {
 		average = sum / count;
@@ -37,5 +31,3 @@ int main() {
 	cout << "average: " << average << " \n";
 	cout << "upper: " << countup << endl;
 	return 0;
-
-}
